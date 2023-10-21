dc88ec63d7625f6f72ddd7a484e8b1d50324ef97
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-21 03:14:25 -0700

    change

--- src/main/java/com/auth/kavish/HomeController.java
+++ src/main/java/com/auth/kavish/HomeController.java
@@ -1,13 +1,13 @@
 package com.auth.kavish;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     String message = "This is a war file from Azure DevOps pipelines!";
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index() {
         return message;
     }
