e981cd44c295dca52bbaab60bd249087f49e404f
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-21 03:43:05 -0700

    try

--- src/main/java/com/auth/kavish/KavishApplication.java
+++ src/main/java/com/auth/kavish/KavishApplication.java
@@ -2,9 +2,17 @@ package com.auth.kavish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KavishApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return ("Welcome to First Spring Boot Application");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KavishApplication.class, args);
