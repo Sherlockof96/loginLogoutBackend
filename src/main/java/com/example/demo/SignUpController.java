2780782f64b282af2cff91daf7519842d878c107
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-22 04:39:53 -0700

    cors

--- src/main/java/com/example/demo/SignUpController.java
+++ src/main/java/com/example/demo/SignUpController.java
@@ -1,16 +1,12 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
@@ -54,7 +50,7 @@ public class SignUpController {
 
         }catch (Exception e)
         {
-            return -1;
+            return 0;
         }
         
         if (user.getId() == id)
