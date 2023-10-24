a135665e4199543e3d5907df8a0f12dbbf013464
sherlockof96 <kavishdoshi28@gmail.com>
2023-10-24 01:04:57 -0700

    session

--- src/main/java/com/example/demo/ProfileRepositorys.java
+++ src/main/java/com/example/demo/ProfileRepositorys.java
@@ -3,10 +3,9 @@ package com.example.demo;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-
 public interface ProfileRepositorys extends CrudRepository<Profile, Integer> {
 
-    @Query("SELECT profile FROM Profile profile WHERE profile.username=:username AND profile.pass=:pass")
+    @Query("SELECT * FROM Profile profile WHERE profile.username=:username AND profile.pass=:pass")
     public Profile findUserByCredentials(@Param("username") String u,
                                         @Param("pass") String p);
 }
